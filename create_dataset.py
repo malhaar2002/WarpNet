3c55402fbc3642b6cc9ba3d7b9cfb67d9a82c511
suhanis16 <suhani.shrivastava@plaksha.edu.in>
2024-05-09 00:24:01 +0530

    Added more textures for training

--- create_dataset.py
+++ create_dataset.py
@@ -4,21 +4,21 @@ import random
 from PIL import Image
 from unstructured_noise import online_add_degradation
 
-input_path = r"D:\WarpNET\coco_persons\val"
-output_path = r"D:\WarpNET\coco_persons\val_degraded"
-filters_path = r"WarpNet\images\old_image_textures"
+input_path = r"D:\WarpNET\coco_persons\B\train"
+output_path = r"D:\WarpNET\coco_persons\A\train"
+filters_path = r"D:\WarpNET\WarpNet\images\old_image_textures"
+
+
 
 # iterate through all images in the input path
 for image_path in os.listdir(input_path):
     image = cv2.imread(os.path.join(input_path, image_path))
     image = cv2.resize(image, (256, 256))
-    if random.uniform(0, 1) < 0.7:
-        # structured defect
-        filter = cv2.imread(os.path.join(filters_path, random.choice(os.listdir(filters_path))))
-        filter = cv2.resize(filter, (256, 256))
-        image = cv2.addWeighted(image, 0.5, filter, 0.5, 0)
-        cv2.imwrite(os.path.join(output_path, image_path), image)
-    else:
-        # unstructured defect
-        image = online_add_degradation(Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)))
-        image.save(os.path.join(output_path, image_path))
+    filter = cv2.imread(os.path.join(filters_path, random.choice(os.listdir(filters_path))))
+    filter = cv2.resize(filter, (256, 256))
+    image = cv2.addWeighted(image, 0.5, filter, 0.5, 0)
+    cv2.imwrite(os.path.join(output_path, image_path), image)
+    # else:
+    #     # unstructured defect
+    #     image = online_add_degradation(Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)))
+    #     image.save(os.path.join(output_path, image_path))
